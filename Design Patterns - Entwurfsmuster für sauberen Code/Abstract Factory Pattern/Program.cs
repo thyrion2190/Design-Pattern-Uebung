22e7d22837103f868f892e6acda076e904419597
bastian raupach <bastian.raupach@bego.com>
2020-08-04 09:51:04 +0200

    Added the Project

---
+++ Design Patterns - Entwurfsmuster für sauberen Code/Abstract Factory Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract_Factory_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Design Patterns - Entwurfsmuster für sauberen Code/Design Patterns - Entwurfsmuster für sauberen Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Patterns___Entwurfsmuster_für_sauberen_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
