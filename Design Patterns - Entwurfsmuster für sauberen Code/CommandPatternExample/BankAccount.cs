ee5631c4e6a88519d2e553d1d3d2a0d4832e2a95
bastian raupach <bastian.raupach@bego.com>
2020-08-08 17:03:20 +0200

    Created CommandPatternExample

---
+++ Design Patterns - Entwurfsmuster für sauberen Code/CommandPatternExample/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPatternExample
+{
+    public class BankAccount
+    {
+        public double Balance { get; set; } = 0;
+        public double OverdraftLimit { get; set; } = 1000;
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            Console.WriteLine("You have deposit: " + amount + "\nNew Bankaccount Balance is: " + Balance);
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+            Console.WriteLine("You have withdraw: " + amount + "\nNew Bankaccount Balance is: " + Balance);
+        }
+
+        public override string ToString()
+        {
+            return "The Bankaccount Balance is: " + Balance + "\nThe Overdraftlimit is: " + OverdraftLimit;
+
+        }
+    }
+}
---
+++ Design Patterns - Entwurfsmuster für sauberen Code/CommandPatternExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
